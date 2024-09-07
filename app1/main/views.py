104953275dd4d1b33bd36b4aebe27bc2380ee110
Alexander Dorofeev <alexandedorofeev@yandex.ru>
2024-09-07 23:10:53 +0300

    everything is set up

--- app1/main/views.py
+++ app1/main/views.py
@@ -4,15 +4,11 @@ from django.shortcuts import render
 
 def index(request):
     context: dict = {
-        'title': 'Home',
-        'content': "Главная страница магазина - HOME",
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'bool': True
+        "title": "Home",
+        "content": "Главная страница магазина - HOME",
+        "list": ["first", "second"],
+        "dict": {"first": 1},
+        "bool": True,
     }
-
-    return render(request, 'main/index.html', context)
     
-
-def about(request):
-    return HttpResponse()
+    return render(request,'main/index.html', context)
